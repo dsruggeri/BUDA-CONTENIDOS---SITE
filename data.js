bad1b0e154ec03ac08ac36895fdf010063d03404
dsruggeri <dsruggeri@gmail.com>
2025-05-14 09:11:14 -0300

    las palabras de la tierra

--- data.js
+++ data.js
@@ -6,20 +6,18 @@ const proyectosData = [{
         url: "lasherramientasdelasoledad.html"
     },
     {
-        titulo: "Naturaleza Muerta",
+        titulo: "Las palabras de la tierra",
+        subtitulo: "La palabra hablada, en resistencia viva",
+        generos: "Largometraje Documental. Tradición Oral. Proyecto en desarrollo",
+        imagen: "/images/thumbLPDLT.png",
+        url: "laspalabrasdelatierra.html"
+    },
+    {
+       titulo: "Naturaleza Muerta",
         subtitulo: "Una película de Pilar Zapata",
         generos: "Cortometraje. Ficción. Drama.",
         imagen: "/images/thumbNM2.png",
         url: "naturalezamuerta.html"
-
-    },
-    {
-        titulo: "Las Herramientas de la soledad",
-        subtitulo: "Una película de Gaspar Gómez",
-        generos: "Largometraje. Ficción. Drama psicológico.",
-        imagen: "/images/thumbLHDLS.png",
-        url: "lasherramientasdelasoledad.html"
-
     },
     {
         titulo: "Las Herramientas de la soledad",
